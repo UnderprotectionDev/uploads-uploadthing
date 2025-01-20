e0b5b94adaa6ee09304cf6123517a3597a0338f1
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-01-20 18:40:59 +0300

    Update dependencies and configure Tailwind CSS with Uploadthing integration
    - Added @uploadthing/react and uploadthing packages to dependencies.
    - Updated package-lock.json to reflect new dependencies.
    - Configured Tailwind CSS to use Uploadthing's utility functions and included tailwindcss-animate plugin.

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,43 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+const auth = (req: Request) => ({ id: "fakeId" });
+
+export const ourFileRouter = {
+  avatarUploader: f({
+    image: {
+      maxFileSize: "2MB",
+      maxFileCount: 1,
+    },
+  })
+    .middleware(async ({ req }) => {
+      const user = await auth(req);
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code runs on your server after upload
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("file url", file.url);
+      return { uploadedBy: metadata.userId };
+    }),
+
+  galleryUploader: f({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 4,
+    },
+  })
+    .middleware(async ({ req }) => {
+      const user = await auth(req);
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("file url", file.url);
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
